e3b3d34855d66910810d5f7b9326bb158c6c373e
Jared Prather <jprather7966@yahoo.com>
2019-07-23 08:51:58 -0500

    Added a pause method

--- snake.py
+++ snake.py
@@ -128,7 +128,23 @@ def handle_events():
                 snake.set_direction_up()
             elif event.key == pygame.K_DOWN:
                 snake.set_direction_down()
-
+            elif event.key == pygame.K_p:
+                pause_game()
+def pause_game():
+    game_is_Paused = True
+    while game_is_Paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT()
+                snake.is_alive == False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    game_is_Paused = False
+                elif event.key == pygame.K_ESCAPE:
+                    snake.is_alive = False
+                    game_is_Paused = False
+            pygame.display.update()
+            clock.tick(5)
 snake = Snake(BLOCK_SIZE * 5, BLOCK_SIZE * 5)
 apple = Apple(snake.body)
 
